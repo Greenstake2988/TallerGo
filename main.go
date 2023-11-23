ec1924ae71c522e68155b8eea533a16df9a1f0df
Oscar Daniel Vazquez Alcocer <oscar.va@valladolid.tecnm.mx>
2023-11-23 15:06:04 -0600

    Ejemplo rutas¨

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	//
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
 )
 
 // album represents data about a record album.
@@ -23,9 +28,50 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(200, albums)
 }
 
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
+	db, err := gorm.Open(sqlite.Open("albums.sqlite"), &gorm.Config{})
+
+	if err != nil {
+		return
+	}
+
+	db.AutoMigrate(&album{})
+
 	r := gin.Default()
 	r.GET("/albums", getAlbums)
+	r.POST("/albums", postAlbums)
+	r.GET("/albums/:id", getAlbumByID)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
